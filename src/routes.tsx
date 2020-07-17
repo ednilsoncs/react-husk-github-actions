20324c7e23deada8bc856f0916ff7ba8333d8364
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-07-17 00:24:09 -0300

    style: configure eslint in project

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home'
+import Home from './pages/Home';
 
-
-export default function Routes(){
-  return(
+const Routes: React.FC = () => {
+  return (
     <Router>
       <Switch>
-        <Route path="/" exact component={Home}/>
+        <Route path="/" exact component={Home} />
       </Switch>
     </Router>
-  )
-}
+  );
+};
+
+export default Routes;
