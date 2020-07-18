0602b600a437023ce76d7ce12fd6756dc1141d78
Ednilson Cardoso <ednilsoncs@dcomp.ufs.br>
2020-07-18 00:10:53 -0300

    Verificar se esta passando no tests

--- src/pages/Home/index.spec.tsx
+++ src/pages/Home/index.spec.tsx
@@ -6,6 +6,6 @@ describe('@Screen/Home', () => {
   it('should render one item content label Hello Ednilson', () => {
     const { getByText } = render(<Home />);
 
-    expect(getByText('Hello Ednilson')).toBeTruthy();
+    expect(getByText('Hello Ednilso')).toBeTruthy();
   });
 });
