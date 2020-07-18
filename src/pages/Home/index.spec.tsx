6f963a3082b57c4d4a9e18a30d81d0b2f219b249
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-07-17 21:01:44 -0300

    chore(project): configure husky

--- src/pages/Home/index.spec.tsx
+++ src/pages/Home/index.spec.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import Home from './index'
 import { render } from '@testing-library/react';
+import Home from './index';
 
-describe('@Screen/Home',()=>{
-  it('should render one item content label Hello Ednilson',()=>{
-    const { getByText } = render(
-        <Home/>
-    );
+describe('@Screen/Home', () => {
+  it('should render one item content label Hello Ednilson', () => {
+    const { getByText } = render(<Home />);
 
-    expect (
-       getByText('Hello Ednilson')
-    ).toBeTruthy()
-  })
-})
+    expect(getByText('Hello Ednilson')).toBeTruthy();
+  });
+});
