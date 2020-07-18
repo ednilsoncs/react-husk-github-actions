5f7e479499d4ff07e6e012ceaf17e80ba53ac178
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-07-18 00:48:34 -0300

    test(project): teste github action deploy

--- src/pages/Home/index.spec.tsx
+++ src/pages/Home/index.spec.tsx
@@ -6,6 +6,10 @@ describe('@Screen/Home', () => {
   it('should render one item content label Hello Ednilson', () => {
     const { getByText } = render(<Home />);
 
-    expect(getByText('Hello Ednilso')).toBeTruthy();
+    expect(
+      getByText(
+        'Hello, this was done or deployed using continuous integration',
+      ),
+    ).toBeTruthy();
   });
 });
